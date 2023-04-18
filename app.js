3606cc84f0e3c0aac9ecb4d697f93f555c9d5b69
sumit <pdsumit07@gmail.com>
2023-04-18 22:52:15 +0530

    connected to mongodb database

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const app = express() ;
+const app = express();
+const mongoose = require("mongoose")
 
-app.listen("3000" , () =>{
-    console.log("the server is hosted at port 3000") ;
+
+const { MONGO_URI } = require("./keys")
+const port = 3000;
+
+mongoose.connect(MONGO_URI)
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongoose");
+});
+mongoose.connection.on('error', (err) => {
+    console.log("error", err);
+});
+//mongoose.connection.on is a method used in Mongoose, an Object-Document Mapper (ODM) for MongoDB, to listen for various events related to the database connection.
+app.listen(port, () => {
+    console.log("the server is hosted at port 3000");
 });
 
-app.get("/", (req,res) =>{
-    res.send("hello world") ;
+app.get("/", (req, res) => {
+    // this is a middleware
+    res.send("hello world");
 });
+
+
+// if we add middleware in as the app.use it will be used after every route but if we need to use it for a specific route we need to pass it
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MONGO_URI : "mongodb+srv://sumit:J2fUm5sQFNsvkrRP@instagram-clone.z6uuefd.mongodb.net/?retryWrites=true&w=majority"
+}
