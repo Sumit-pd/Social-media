9e1320eb487ffb0d6ee9077c5c21e76a8c3d2da9
sumit <pdsumit07@gmail.com>
2023-04-20 11:02:06 +0530

    save or post data to mongo database

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,38 @@
 //we should always try to create routes in different folder 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+
+
+const User = mongoose.model('User',)
 
 router.get('/', (req, res) => {
     res.send("hello World");
 });
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body; // this is something we get from the frontend part
-    if(!name || !email || !password ){
-        return res.status(422).json({error:"please fill all the details"})
+    if (!name || !email || !password) {
+        return res.status(422).json({ error: "please fill all the details" })
         //status 422 means that the server has understood the details but cannot process it
     }
-    else{
-        res.json({message : "succesfully sent"})
-    }
+    // we are checking if the email provided from the frontend is already taken by a existing user
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (savedUser) {
+                // we found the user so we will return an error
+                return res.status(422).json({ "error": "the email is already taken" });
+            }
+            const user = new User({
+                email,
+                name,
+                password
+            });
+            user.save()
+                .then(user => {
+                    res.json({ "message": "user save sucessfully" })
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 });
 module.exports = router
